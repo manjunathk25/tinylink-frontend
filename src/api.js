ed8039488f79c07801abbcd7570c30128e9167d4
manjunath25 <k.mnjunth@gmail.com>
2025-11-23 16:46:09 +0530

    Creating .env file and importing base url from there"

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://tnylnk.up.railway.app';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const createShortLink = ({url, customCode}) => axios.post(`${BASE_URL}/api/links`, { url, customCode });
 export const getAllLinks = () => axios.get(`${BASE_URL}/api/links`);
